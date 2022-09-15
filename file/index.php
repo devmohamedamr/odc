aa227060bd0b7e6c3128b67d26af68c5bc420289
mohamed amr <dev.mohamedamr@gmail.com>
2022-09-15 19:35:47 +0200

    cer

--- file/index.php
+++ file/index.php
@@ -2,7 +2,7 @@
 
 
 $list_of_students =  explode(",",$_POST['students']);
-
+$course = $_POST['course'];
 $content = file_get_contents("certificate.html");
 
 
@@ -10,7 +10,7 @@ foreach($list_of_students as $key => $student){
     $location = "cer/".$student.".html";
     fopen($location,"w");
 
-    $new_content =  str_replace("name",$student,$content);
+    $new_content =  str_replace(["name","course"],[$student,$course],$content);
 
     file_put_contents($location,$new_content);
 }
