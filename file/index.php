39eef474ada310da23a011e6061faa8e0335f6f4
mohamed amr <dev.mohamedamr@gmail.com>
2022-09-15 20:01:23 +0200

    cer

--- file/index.php
+++ file/index.php
@@ -1,16 +1,37 @@
 <?php
 
-
-$list_of_students =  explode(",",$_POST['students']);
-$course = $_POST['course'];
-$content = file_get_contents("certificate.html");
+$errors = [];
 
 
-foreach($list_of_students as $key => $student){
-    $location = "cer/".$student.".html";
-    fopen($location,"w");
-
-    $new_content =  str_replace(["name","course"],[$student,$course],$content);
-
-    file_put_contents($location,$new_content);
+if($_POST['students'] == '' || empty($_POST['students']) || strlen($_POST['students']) == 0){
+    $errors[] = "students must be required";
 }
+
+
+if($_POST['course'] == '' || empty($_POST['course']) || strlen($_POST['course']) == 0){
+    $errors[] = "course must be required";
+}
+
+
+
+if(count($errors) == 0){
+    $list_of_students =  explode(",",$_POST['students']);
+    $course = $_POST['course'];
+    $content = file_get_contents("certificate.html");
+    
+    
+    foreach($list_of_students as $key => $student){
+        $location = "cer/".$student.".html";
+        fopen($location,"w");
+    
+        $new_content =  str_replace(["name","course"],[$student,$course],$content);
+    
+        file_put_contents($location,$new_content);
+    }
+}else{
+    foreach($errors as $error){
+        echo $error."<br>";
+    }
+}
+
+
